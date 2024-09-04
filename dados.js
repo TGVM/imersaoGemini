9f69b92a03d84e709ac53938c005f26d17985c6a
TGVM <tgvm96@gmail.com>
2024-09-04 11:31:39 -0300

    Aula 03: Criando a base de dados com JavaScript

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+      titulo: "Rayssa Leal",
+      descricao: "Raíssa Leal, a Fadinha, é uma das maiores promessas do skate feminino brasileiro. Ainda jovem, ela já conquistou diversos títulos e se tornou um exemplo para jovens skatistas.",
+      link: "https://pt.wikipedia.org/wiki/Raissa_Leal"
+    },
+    {
+      titulo: "Beatriz Souza",
+      descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024, sendo um dos maiores destaques do judô feminino brasileiro.",
+      link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    },
+    {
+      titulo: "Rebeca Andrade",
+      descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou diversas medalhas olímpicas, incluindo o ouro nos Jogos Olímpicos de Paris 2024, sendo um dos maiores nomes da ginástica artística brasileira.",
+      link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade" 
+    }
+  ];
