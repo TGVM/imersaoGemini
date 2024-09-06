8dd12976ecc6f6d4c53ff85f1468067fb5424d77
TGVM <tgvm96@gmail.com>
2024-09-06 10:41:13 -0300

    Aula 05: Finalizando o projeto com GitHub e Deploy

--- dados.js
+++ dados.js
@@ -2,16 +2,19 @@ let dados = [
     {
       titulo: "Rayssa Leal",
       descricao: "Raíssa Leal, a Fadinha, é uma das maiores promessas do skate feminino brasileiro. Ainda jovem, ela já conquistou diversos títulos e se tornou um exemplo para jovens skatistas.",
-      link: "https://pt.wikipedia.org/wiki/Raissa_Leal"
+      link: "https://pt.wikipedia.org/wiki/Raissa_Leal",
+      tags: "skate fada sk8"
     },
     {
       titulo: "Beatriz Souza",
       descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024, sendo um dos maiores destaques do judô feminino brasileiro.",
-      link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+      link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+      tags: "judô judo judoka ouro"
     },
     {
       titulo: "Rebeca Andrade",
       descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou diversas medalhas olímpicas, incluindo o ouro nos Jogos Olímpicos de Paris 2024, sendo um dos maiores nomes da ginástica artística brasileira.",
-      link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade" 
+      link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+      tags: "ginástica ginastica ouro"
     }
   ];
